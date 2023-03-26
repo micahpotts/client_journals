c191f8591594cb4b4473f8dae33e1257332a7595
Micah Potts <mrs.micahpotts@gmail.com>
2023-03-26 18:11:06 -0400

    journal entry controller test

--- test/controllers/journal_entries_controller_test.rb
+++ test/controllers/journal_entries_controller_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class JournalEntriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get entries by provider" do
+    provider_id = providers(:one).id
+    get "/providers/#{provider_id}/journal_entries"
+    assert_response :success
+  end
+  test "get entries by client" do
+    client_id = clients(:one).id
+    get "/clients/#{client_id}/journal_entries"
+    assert_response :success
+  end
 end
