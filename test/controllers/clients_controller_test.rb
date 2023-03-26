68c17b9302a47d99059221b6718d601f782451ec
Micah Potts <mrs.micahpotts@gmail.com>
2023-03-26 18:05:25 -0400

    added client controller tests for the happiest of paths

--- test/controllers/clients_controller_test.rb
+++ test/controllers/clients_controller_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class ClientsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+   test "see all clients" do
+     get "/clients"
+     assert_response :success
+   end
+   test "get client by id" do
+     client_id = clients(:one).id
+     get "/clients/#{client_id}"
+     assert_response :success
+   end
+   test "create client" do
+     new_client = {"name": "New Girl", "email": "its@jess.com", "plan": "basic"}
+     post "/clients", params: new_client
+     assert_response :success
+   end
+   test "update providers" do
+     client_id = clients(:one).id
+     provider_1 = providers(:one).id
+     provider_2 = providers(:two).id
+     provider_3 = providers(:three).id
+     patch "/clients/#{client_id}/providers", params: {"providers": [provider_1, provider_2, provider_3]}
+     assert_response :success
+   end
+   test "get clients by provider" do
+     provider_id = providers(:one).id
+     get "/providers/#{provider_id}/clients"
+     assert_response :success
+   end
 end
